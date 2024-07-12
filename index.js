5b049916f7be120a40ae218a86bbc75e862f8daf
shiva-raj-kc <152582537+shiva-raj-kc@users.noreply.github.com>
2024-07-12 20:13:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
+const {createTodo,updateTodo} = require('./types')
+// app.post('/signup',(req,res)=>{
+
+// })
+// app.post('/signin',(req,res)=>{
+
+// })
+app.post('/todo',(req,res)=>{
+    const createPayload = req.body
+    const parsedPayload = createTodo.safeParse(createPayload)
+    if(!parsedPayload.success){
+        return res.status(411).json({
+            msg : 'invalid input'
+        })
+    }
+    return
+
+})
+app.get('/todos',(req,res)=>{
+
+})
+app.put('/completed',(req,res)=>{
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg : 'requires valid input'
+        })
+    }
+    return
+})
+
+
+
+
+app.listen(3000)
